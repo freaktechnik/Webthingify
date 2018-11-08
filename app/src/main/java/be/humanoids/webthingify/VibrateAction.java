1171f1ed2927142c52a419b80430ae429a497fdd
Michael Stegeman <michael@stegeman.me>
2018-11-08 06:47:19 -0700

    Fix vibrate action.

--- app/src/main/java/be/humanoids/webthingify/VibrateAction.java
+++ app/src/main/java/be/humanoids/webthingify/VibrateAction.java
@@ -2,11 +2,12 @@ package be.humanoids.webthingify;
 
 import org.json.JSONObject;
 import org.mozilla.iot.webthing.Action;
+import org.mozilla.iot.webthing.Thing;
 
 import java.util.UUID;
 
 class VibrateAction extends Action {
-    public VibrateAction(Phone thing, JSONObject input) {
+    public VibrateAction(Thing thing, JSONObject input) {
         super(UUID.randomUUID().toString(), thing, "vibrate", input);
     }
 
